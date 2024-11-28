4f63989a9be813b1f1de98c342e735ad4d35949d
enzosalles <enzosalles@CQI-BNE-2525>
2024-11-28 10:32:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/TesteMesa1.cs
@@ -0,0 +1,13 @@
+﻿public class TesteMesa1
+{
+    static void Main(string[] args)
+    {
+        int[] v = new int[5];
+        int a = 10;
+        int b = 20;
+        int c = (a + b) / 2;
+        c = c - 40;
+        v[3] = a + b + c;
+        Console.WriteLine("O resultado de v[3] é " + v[3]);
+    }
+}
