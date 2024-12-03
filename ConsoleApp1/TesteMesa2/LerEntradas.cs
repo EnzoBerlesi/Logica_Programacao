60875d0206e7e68798a951c565b4dbf3f300585a
Enzo Salles - E. Escolinha de TI - BNE <enzosalles@employer.matriz>
2024-12-03 09:07:53 -0300

    Mensagem do commit

---
+++ ConsoleApp1/TesteMesa2/LerEntradas.cs
@@ -0,0 +1,26 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+        public class LerEntradas
+        {
+            public static void Entradas() // exercicio 3
+            {
+                LerEntradas lerEntrada = new LerEntradas();
+
+                Console.WriteLine("Digite o valor total: ");
+                double valorAtual = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite o valor de juros: ");
+                double juros = Convert.ToDouble (Console.ReadLine());
+                Console.WriteLine("Digite o valor periodo(meses): ");
+                double periodo = Convert.ToDouble(Console.ReadLine());
+
+                double resultado = valorAtual * Math.Pow(1 + juros, periodo);
+
+                Console.WriteLine(valorAtual + juros + periodo);
+                Console.WriteLine("O valor final após " + periodo + " meses é: " + resultado);
+        }
+        }
+    
